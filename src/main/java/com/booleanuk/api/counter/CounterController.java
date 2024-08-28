7199e2cccde4506b59c5e01901c8cf043804f4cf
Dag Andreas <daanfofo@gmail.com>
2024-08-28 11:21:56 +0200

    completed core with inc and dec buttons

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,39 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("counter")
 public class CounterController {
+	public int counter;
+
+
+	public CounterController(){
+		counter = 0;
+	}
+
+	@GetMapping()
+	public String getCounter(){
+		return withDivAndLink(Integer.toString(counter));
+	}
+
+	@GetMapping("increment")
+	public String increment(){
+		return withDivAndLink(Integer.toString(++counter));
+	}
+
+	@GetMapping("decrement")
+	public String decrement(){
+		return withDivAndLink(Integer.toString(--counter));
+	}
+
+	public String withDivAndLink(String s){
+		return "<div>" + s + "</div>"
+			+"<div><a href=\"http://localhost:4000/counter/increment\">Increment Counter</a></div>"
+			+ "<div><a href=\"http://localhost:4000/counter/decrement\">Decrement Counter</a></div>";
+	}
+
 
 }
